12ebc8c7da5043be0e64a0d840384f6f2c7b3ce2
Nurtal <nathan.foulquier.pro@gmail.com>
2024-08-20 19:15:48 +0200

    init

--- trash.py
+++ trash.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 
@@ -11,11 +12,32 @@ def stupid_approach():
 
 
 
+def generate_deviation_plot():
+    """ """
+    
+    # load data
+    df = pd.read_csv("data/data.csv")
+    df.set_index('ID')
+    df = df.drop(columns=['ID'])
+
+    # normalize
+    # df=(df-df.min())/(df.max()-df.min())
+    df=(df-df.mean())/df.std()
+    df = df.replace(np.nan, 0)
+    
+    # replace by ecart à la moyenne
+    df = df-df.mean()
+    print(df)
+
+    # reorder
+
+    
+
 
 
 if __name__ == "__main__":
 
 
-    stupid_approach()
+    generate_deviation_plot()
 
     
