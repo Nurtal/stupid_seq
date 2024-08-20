ad1510ddb74f17031eb4c16ebc56d41be38fb1de
Nurtal <nathan.foulquier.pro@gmail.com>
2024-08-20 18:51:02 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# STUPID SEQ
+
+## Overview
+pet project, use sequence alignement to find pattern & perform clustering
---
+++ trash.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+
+def stupid_approach():
+    """ """
+
+    # load data
+    df = pd.read_csv("data/data.csv")
+    print(df)
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    stupid_approach()
+
+    
